e8dc1e711b439ecb34dac9fc3d7be5765202b9a8
babaliaris <babaliaris.nikos@gmail.com>
2025-03-22 17:01:28 +0200

    Finished Lesson: Vertex Attributes

--- projects/Sandbox/src/lessons/lessons.h
+++ projects/Sandbox/src/lessons/lessons.h
@@ -3,18 +3,23 @@
 #include "chapter2/a_our_first_application.h"
 #include "chapter2/b_using_shaders.h"
 #include "chapter2/c_drawing_our_first_triangle.h"
+#include "chapter3/a_vertex_attributes.h"
 
 enum class LessonE
 {
     NONE, CHAPTER2_OUR_FIRST_APPLICATION,
     CHAPTER2_USING_SHADERS,
-    CHAPTER2_DRAWING_OUR_FIRST_TRIANGLE
+    CHAPTER2_DRAWING_OUR_FIRST_TRIANGLE,
+    CHAPTER3_VERTEX_ATTRIBUTES
 };
 
 Lesson *GetLesson(LessonE lesson)
 {
     switch (lesson)
     {
+        case LessonE::CHAPTER3_VERTEX_ATTRIBUTES:
+            return new VertexAttributes();
+
         case LessonE::CHAPTER2_DRAWING_OUR_FIRST_TRIANGLE:
             return new DrawingOurFirstTriangle();
 
